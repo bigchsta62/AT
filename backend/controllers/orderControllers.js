1ae2b72a45631a005f232d71ea58f080d8e13069
Anthony Belcastro <belcastt@live.com>
2020-11-30 17:43:41 -0700

    creation of an order Controller to post to the db

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,27 @@
+
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+
+
+
+// the is going to create a new order made
+// this will be the Post to /api/orders
+// this is access for a protected routes
+const addOrderItems = asyncHandler(async (req, res) => {
+    const { addOrderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Erro('No order items')
+        return
+
+    } else {
+        const order = new Order({
+            addOrderItems, user: req.user._id, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice
+
+        })
+
+        const createdOrder = await order.save()
+        res.status(201).json({ createdOrder })
+    }
+})
